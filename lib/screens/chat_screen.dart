24287b8d2622ed256049f931d35a055792d82033
Gabriel Souza <gabrielss_@outlook.com>
2021-03-01 17:22:19 -0300

    até auth

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlutterChat'),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 8),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              )
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance
+                    .signOut(); // Faz com que o usuário se deslogue
+              }
+            },
+          )
+        ],
+      ),
+      // sempre que a stream der um novo value, executa a função do builder
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/UJk9mO1K1RkeWrHrG8Ov/messages')
+            .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          // função a ser executada
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/UJk9mO1K1RkeWrHrG8Ov/messages')
+              .add({'text': 'This was added by clicking the button!'});
+        },
+      ),
+    );
+  }
+}
---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  final bool isLoading;
+  final void Function(
+    String email,
+    String password,
+    String username,
+    bool isLogin,
+    BuildContext ctx,
+  ) submitFn;
+
+  AuthForm(this.submitFn, this.isLoading);
+
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  var _isLogin = true;
+  final _formKey = GlobalKey<FormState>();
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus(); // Fechar o teclado quando validar
+
+    if (isValid) {
+      _formKey.currentState.save();
+      widget.submitFn(
+        _userEmail.trim(),
+        _userPassword.trim(),
+        _userName.trim(),
+        _isLogin,
+        context,
+      );
+      // Use the values to send a auth request to firebase
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    key: ValueKey('email'),
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _userEmail = value;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: 'Email address'),
+                  ),
+                  if (!_isLogin) //Se não estiver na tela de login não mostra username
+                    TextFormField(
+                      key: ValueKey('username'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characters.';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _userName = value;
+                      },
+                      decoration: InputDecoration(labelText: 'Username'),
+                    ),
+                  TextFormField(
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _userPassword = value;
+                    },
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 12),
+                  widget.isLoading
+                      ? CircularProgressIndicator()
+                      : RaisedButton(
+                          child: Text(_isLogin ? 'Login' : 'Signup'),
+                          onPressed: _trySubmit,
+                        ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(_isLogin
+                        ? 'Create a new account'
+                        : 'I already have an account'),
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
